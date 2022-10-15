8d0ba8198daf654db27067cbe826f97778a8c3ee
paoloevan <paoloev1900@gmail.com>
2022-10-15 13:07:47 +0200

    add pari e dispari ex

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,12 @@
 Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
-
-Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer
-(usando una funzione). Sommiamo i due numeri.
-Stabiliamo se la somma dei due numeri è pari o dispari
-(usando una funzione) Dichiariamo chi ha vinto.
 */
-
-/* PALINDROMA */
 
 //inserire la parola
 const userWord = prompt('inserisci una parola');
 
-palindroma (userWord);
+palindroma(userWord);
 
 
 //creo la funzione
@@ -44,4 +35,52 @@ function palindroma(Word) {
     } else {
         console.log('La parola non è palindroma');
     }
+}
+
+/*
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer
+(usando una funzione). 
+Sommiamo i due numeri.
+Stabiliamo se la somma dei due numeri è pari o dispari
+(usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+//utente sceglie pari o dispari
+const pariDispari = prompt('Pari o dispari?');
+
+//utente inserisce il numero
+const userNumber = Number(prompt('Inserisci un numero da 1 a 5'));
+
+//genero numero per pc con funzione
+function randomNumber() {
+    const number = Math.floor(Math.random() * 5) + 1;
+
+    return number
+}
+const pcNumber = randomNumber();
+console.log('pc number', pcNumber);
+
+//sommo i due numeri
+const numberSum = userNumber + pcNumber;
+
+//funzione per pari o dispari
+function resultNumber(numOne) {
+    //stabilisco se somma è pari o dispari
+    if (numOne % 2 == 0) {
+        return 'pari'
+    }
+    return 'dispari'
+}
+
+// invoco la funzione
+const gameResult = resultNumber(numberSum)
+//console.log(gameResult);
+
+if (pariDispari == gameResult) {
+    console.log('Hai vinto!');
+} else {
+    console.log('Hai perso :(');
 }
